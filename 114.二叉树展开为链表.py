2bd8e65e034779b8f5679d1c52abfcaf56467bc0
KaiyeZhou <243974648@qq.com>
2019-09-05 22:02:39 +0800

    LeetCode

---
+++ 114.二叉树展开为链表.py
@@ -0,0 +1,66 @@
+'''
+给定一个二叉树，原地将它展开为链表。
+
+例如，给定二叉树
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+将其展开为：
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        # 递归，后续遍历
+        self.flatten(root.left)
+        self.flatten(root.right)
+        if root.left:
+            right = root.right
+            root.right = root.left
+            root.left = None
+            # 右子树接原左子树的最右叶子节点
+            while root.right:
+                root = root.right
+            root.right = right
+        
+        # 迭代
+        cur = root
+        while cur:
+            if cur.left:
+                p = cur.left
+                while p.right:
+                    p = p.right
+                p.right = cur.right
+                cur.right = cur.left
+                cur.left = None
+            cur = cur.right
