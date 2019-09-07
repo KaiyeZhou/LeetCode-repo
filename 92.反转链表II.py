b73c9f32fb996a8b1978788055f91959278b291b
KaiyeZhou <243974648@qq.com>
2019-09-07 16:13:29 +0800

    LeetCode

--- 92.反转链表II.py
+++ 92.反转链表II.py
@@ -1,3 +1,19 @@
+'''
+反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+
+说明:
+1 ≤ m ≤ n ≤ 链表长度。
+
+示例:
+
+输入: 1->2->3->4->5->NULL, m = 2, n = 4
+输出: 1->4->3->2->5->NULL
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-linked-list-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -43,4 +59,21 @@ class Solution:
         #     first = first.next
         # return res.next
 
-            
+        # solution 3
+        if head == None or head.next == None or m >= n:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        start = dummy
+        for i in range(m - 1):
+            start = start.next
+        end = cur = start.next
+        pre = None
+        for i in range(n - m + 1):
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        start.next = pre
+        end.next = cur
+        return dummy.next
