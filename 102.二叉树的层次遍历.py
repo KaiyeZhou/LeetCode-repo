34eac60f210985747bee260a8832dda73d8b0807
KaiyeZhou <243974648@qq.com>
2019-09-25 22:46:19 +0800

    LeetCode

---
+++ 102.二叉树的层次遍历.py
@@ -0,0 +1,65 @@
+'''
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+        stack = [root]
+        res = []
+        while stack:
+            tmpres = []
+            tmplist = []
+            for node in stack:
+                tmpres.append(node.val)
+                if node.left:
+                    tmplist.append(node.left)
+                if node.right:
+                    tmplist.append(node.right)
+            res.append(tmpres)
+            stack = tmplist
+        return res
+
+        # BFS
+        if not root:
+            return []
+        levels = []
+        
+        def helper(node, level):
+            if len(levels) == level:
+                levels.append([])
+            levels[level].append(node.val)
+            if node.left:
+                helper(node.left, level + 1)
+            if node.right:
+                helper(node.right, level + 1)
+        
+        helper(root, 0)
+        return levels
